ecce2edf45371c0f2b220de3a52fa5d7e4c78355
Babak Alizadeh <alizadeh.babak@gmail.com>
2025-03-21 19:22:29 +0000

    bug fix in service workers

--- webUI/src/registerServiceWorker.js
+++ webUI/src/registerServiceWorker.js
@@ -1,6 +1,6 @@
 if('serviceWorker' in navigator) {
     navigator.serviceWorker
-        .register('/service-worker.js').then(registration => {
+        .register('/u/service-worker.js').then(registration => {
             console.log('Service worker registration succeeded:' , registration);
         }).catch(err => {
             console.log('Service worker registration failed:' , err);
