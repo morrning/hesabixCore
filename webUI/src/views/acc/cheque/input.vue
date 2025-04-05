3908830417f550c76aca4c84c52274bc3f70e856
Babak Alizadeh <alizadeh.babak@gmail.com>
2025-04-05 22:25:49 +0000

    progress in cheque

--- webUI/src/views/acc/cheque/input.vue
+++ webUI/src/views/acc/cheque/input.vue
@@ -1,5 +1,5 @@
 <template>
-   <v-toolbar color="toolbar" :title="$route.params.id ? 'ویرایش چک دریافتی' : 'ثبت چک دریافتی'">
+  <v-toolbar color="toolbar" :title="$route.params.id ? 'ویرایش چک دریافتی' : 'ثبت چک دریافتی'">
     <template v-slot:prepend>
       <v-tooltip :text="$t('dialog.back')" location="bottom">
         <template v-slot:activator="{ props }">
@@ -11,72 +11,48 @@
     <template v-slot:append>
       <v-tooltip :text="$route.params.id ? 'ویرایش' : 'ثبت'" location="bottom">
         <template v-slot:activator="{ props }">
-          <v-btn
-            v-bind="props"
-            color="success"
-            @click="submitForm"
-            :loading="loading"
-            icon="mdi-content-save"
-          />
+          <v-btn v-bind="props" color="success" @click="submitForm" :loading="loading" icon="mdi-content-save" />
         </template>
       </v-tooltip>
     </template>
   </v-toolbar>
-  <v-card>
-    <v-card-title class="text-h5">
-      {{ $route.params.id ? 'ویرایش چک دریافتی' : 'ثبت چک دریافتی' }}
-    </v-card-title>
+  <v-container>
+    <v-row>
+      <v-col cols="12">
+        <v-form @submit.prevent="submitForm">
+          <v-row>
+            <v-col cols="12" md="6">
+              <Hpersonsearch v-model="form.personId" label="شخص" :rules="[v => !!v || 'شخص الزامی است']" required>
+              </Hpersonsearch>
+            </v-col>
+            <v-col cols="12" md="6">
+              <v-text-field v-model="form.chequeNumber" label="شماره چک" :rules="[v => !!v || 'شماره چک الزامی است']"
+                required></v-text-field>
+            </v-col>
 
-    <v-card-text>
-      <v-form @submit.prevent="submitForm">
-        <v-row>
-          <v-col cols="12" md="6">
-                <Hpersonsearch v-model="form.personId" label="شخص" :rules="[v => !!v || 'شخص الزامی است']" required></Hpersonsearch>
-          </v-col>
-          <v-col cols="12" md="6">
-            <v-text-field
-              v-model="form.chequeNumber"
-              label="شماره چک"
-              :rules="[v => !!v || 'شماره چک الزامی است']"
-              required
-            ></v-text-field>
-          </v-col>
+            <v-col cols="12" md="6">
+              <v-text-field v-model="form.bankName" label="نام بانک" :rules="[v => !!v || 'نام بانک الزامی است']"
+                required></v-text-field>
+            </v-col>
 
-          <v-col cols="12" md="6">
-            <v-text-field
-              v-model="form.bankName"
-              label="نام بانک"
-              :rules="[v => !!v || 'نام بانک الزامی است']"
-              required
-            ></v-text-field>
-          </v-col>
+            <v-col cols="12" md="6">
+              <v-text-field v-model="form.amount" label="مبلغ" type="number" :rules="[v => !!v || 'مبلغ الزامی است']"
+                required></v-text-field>
+            </v-col>
 
-          <v-col cols="12" md="6">
-            <v-text-field
-              v-model="form.amount"
-              label="مبلغ"
-              type="number"
-              :rules="[v => !!v || 'مبلغ الزامی است']"
-              required
-            ></v-text-field>
-          </v-col>
+            <v-col cols="12" md="6">
+              <Hdatepicker v-model="form.dueDate" label="تاریخ سررسید" :rules="[v => !!v || 'تاریخ سررسید الزامی است']"
+                required></Hdatepicker>
+            </v-col>
 
-          <v-col cols="12" md="6">
-            <Hdatepicker v-model="form.dueDate" label="تاریخ سررسید" :rules="[v => !!v || 'تاریخ سررسید الزامی است']"
-              required></Hdatepicker>
-          </v-col>
-
-          <v-col cols="12">
-            <v-textarea
-              v-model="form.description"
-              label="توضیحات"
-              rows="3"
-            ></v-textarea>
-          </v-col>
-        </v-row>
-      </v-form>
-    </v-card-text>
-  </v-card>
+            <v-col cols="12">
+              <v-textarea v-model="form.description" label="توضیحات" rows="3"></v-textarea>
+            </v-col>
+          </v-row>
+        </v-form>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
